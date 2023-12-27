53a225c0cab354b66753c87d514d7d5c5b005852
satou haruka <haruka.satou.37@icloud.com>
2023-12-27 13:24:36 +0900

    🐄🍈 UI・バグ修正、画像プレビュー追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :address, :body, tag_ids: [])
+    params.require(:post).permit(:title, :address, :body, tag_ids: [], images: [] )
   end
 
   def set_post
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    screens: {
+      'sm': '440px', //ビューでsm:クラスとすると440以上でsm:クラスを適応。元のクラスをスマホ用にしておく
+    },
+  },
 	plugins: [require("daisyui")],
 
   daisyui: {
