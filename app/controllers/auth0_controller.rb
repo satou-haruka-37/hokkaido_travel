5b00820a6b286c8a1608f069b28ae86d3cac11c8
satou haruka <haruka.satou.37@icloud.com>
2023-12-27 17:55:04 +0900

    :shrimp: 全然関係ないけどログイン・ログアウトのフラッシュメッセージ

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -3,11 +3,13 @@ class Auth0Controller < ApplicationController
   def callback
     user = User.find_or_create_from_auth(request.env["omniauth.auth"])
     session[:user_id] = user.uid
+    flash[:info] = "ログインしました"
     redirect_to request.env["omniauth.origin"] || root_url
   end
 
   def logout
     reset_session
+    flash[:info] = "ログアウトしました"
     request_params = {
       returnTo: root_url,
       client_id: Rails.application.credentials.auth0.client_id,
