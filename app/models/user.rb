7ea4c9ded4ba9ecf8c7aba40757df30fcbe67c2c
satou haruka <haruka.satou.37@icloud.com>
2023-12-29 18:58:33 +0900

    :bear: #146 ログインするたびに、名前が上書きされてしまうバグを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   def self.find_or_create_from_auth(auth)
     uid = auth[:uid]
     user = find_or_initialize_by(uid:) # UIDでユーザーを検索または新規作成（まだ保存されていない）
-    user.name = auth[:info][:name] # Auth0からの情報をユーザーに追加
+    user.name = auth[:info][:name] if user.new_record?# Auth0からの情報をユーザーに追加
     user.save if user.changed? # 変更がある場合のみ保存
     user
   end
