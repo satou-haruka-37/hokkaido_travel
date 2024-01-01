448312e28dbc5a812e21647c8b9d193f7744f549
satou haruka <haruka.satou.37@icloud.com>
2024-01-01 12:52:35 +0900

    :cow2: マイページのお気に入り一覧からもお気に入り解除できるよう修正解除できるようにする

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,6 +8,10 @@ class BookmarksController < ApplicationController
   def destroy
     post = current_user.bookmarks_posts.find(params[:post_id])
     current_user.bookmark_destroy(post)
-    redirect_to post_path(post)
+    if request.referer == mypage_bookmark_posts_url
+      redirect_to mypage_bookmark_posts_path
+    else
+      redirect_to post_path(post)
+    end
   end
 end
