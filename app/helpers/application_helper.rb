03cd4ae2b1526f9c5a07b422f149a0f0d572223a
satou haruka <haruka.satou.37@icloud.com>
2024-01-05 17:34:24 +0900

    :cow2: #58 北海道への行き方検索ツール（GoogleMap使用）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def google_maps_directions_url(destination)
+    "https://www.google.com/maps/dir/?api=1&origin=現在地&destination=#{destination}&travelmode=transit"
+  end
 end
