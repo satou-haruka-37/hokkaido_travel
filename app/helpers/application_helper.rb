2f23259258f0f5397110aa845e67abd42a04df4c
satou haruka <haruka.satou.37@icloud.com>
2024-01-05 18:11:52 +0900

    :shrimp: 遷移先のGooglemapの検索結果を「おすすめの移動手段」に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def google_maps_directions_url(destination)
-    "https://www.google.com/maps/dir/?api=1&origin=現在地&destination=#{destination}&travelmode=transit"
+    "https://www.google.com/maps/dir/?api=1&origin=現在地&destination=#{destination}"
   end
 end
