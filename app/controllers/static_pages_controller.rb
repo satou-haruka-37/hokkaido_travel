f9133943807ac7e6155273235b76e7d22d99790a
satou haruka <haruka.satou.37@icloud.com>
2024-01-05 13:18:54 +0900

    :bear: safariだと「道民はボタンを押してチェック」ボタンが押せないバグ修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,6 @@ class StaticPagesController < ApplicationController
   def privacy_policy; end
 
   def terms_of_service; end
+
+  def location_trouble; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/privacy_policy', to: 'static_pages#privacy_policy'
   get '/terms_of_service', to: 'static_pages#terms_of_service'
+  get '/location_trouble', to: 'static_pages#location_trouble'
 
   get '/auth/:provider/callback', to: 'auth0#callback'
   get '/auth/failure', to: 'auth0#failure'
