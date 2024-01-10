9eda7ea6f7cebbb342247d73e34d67ca7af300f5
satou haruka <haruka.satou.37@icloud.com>
2024-01-11 01:05:39 +0900

    :cow2: #104 Metaタグ・OGP設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,28 @@
 module ApplicationHelper
-  def google_maps_directions_url(destination)
-    "https://www.google.com/maps/dir/?api=1&origin=現在地&destination=#{destination}"
+  def default_meta_tags
+    {
+      site: 'えぞとらべる',
+      title: '北海道を、もっと身近に。',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'えぞとらべるは、北海道の新たな魅力を発見し、北海道旅行のお悩みを解決するサービスです。',
+      keywords: '北海道,北海道旅行,北海道旅,旅行,旅,ほっかいどう,ほっかいどうりょこう,えぞとらべる,エゾトラベル.えぞトラベル,道内旅行,グルメ,観光,北海道観光,宿,宿泊,ホテル,旅館,ランチ,ディナー,ごはん,レジャー',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@lemonade_37',
+        image: image_url('ogp.png')
+      }
+    }
   end
 end
