f6ce90754ea8e31dd927bafb309c9696c6c0acbf
satou haruka <haruka.satou.37@icloud.com>
2024-01-11 03:27:39 +0900

    :cow2: 動的OGP画像とタイトル

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,47 @@
 module ApplicationHelper
-  def default_meta_tags
-    {
-      site: 'えぞとらべる',
-      title: '北海道を、もっと身近に。',
+  def show_meta_tags
+    assign_meta_tags if display_meta_tags.blank?
+    display_meta_tags
+  end
+
+  def assign_meta_tags(options = {})
+    defaults = t('meta_tags.defaults')
+    options.reverse_merge!(defaults)
+    site = options[:site]
+    title = options[:title]
+    description = options[:description]
+    keywords = options[:keywords]
+    image = options[:image].presence || image_url('ogp.png')
+    configs = {
+      site:,
+      title:,
       reverse: true,
       charset: 'utf-8',
-      description: 'えぞとらべるは、北海道の新たな魅力を発見し、北海道旅行のお悩みを解決するサービスです。',
-      keywords: '北海道,北海道旅行,北海道旅,旅行,旅,ほっかいどう,ほっかいどうりょこう,えぞとらべる,エゾトラベル.えぞトラベル,道内旅行,グルメ,観光,北海道観光,宿,宿泊,ホテル,旅館,ランチ,ディナー,ごはん,レジャー',
+      description:,
+      keywords:,
       canonical: request.original_url,
       separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+        { href: image_url('icon.png'), rel: 'icon', sizes: '36x36', type: 'image/png' }, # 36x36ピクセルのAndroid Touch Icon
+        { href: image_url('icon.png'), rel: 'icon', sizes: '48x48', type: 'image/png' }, # 48x48ピクセルのAndroid Touch Icon
+      ],
       og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
+        site_name: site,
+        title: title.presence || t('meta_tags.defaults.title'),
+        description:,
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp.png'),
+        image:,
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary_large_image',
         site: '@lemonade_37',
-        image: image_url('ogp.png')
+        image:
       }
     }
+    set_meta_tags(configs)
   end
 end
