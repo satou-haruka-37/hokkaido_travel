1991d05c9fe76a8b5145084935e8b4fb424d55e2
はる <haruka.satou.37@icloud.com>
2024-01-25 20:07:41 +0900

    Revert ":melon: 開発者からのお知らせ一覧に文章少し表示"

--- app/helpers/markdown_to_html_helper.rb
+++ app/helpers/markdown_to_html_helper.rb
@@ -4,15 +4,4 @@ module MarkdownToHtmlHelper
     markdown = Redcarpet::Markdown.new(renderer)
     markdown.render(text).html_safe
   end
-
-  # お知らせ一覧画面でのお知らせ本文の部分的な表示
-  def markdown_to_text(text, limit)
-    renderer = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(renderer)
-
-    # マークダウンをHTMLに変換してから、マークダウン記法やHTMLタグなどを除去
-    plain_text = strip_tags(markdown.render(text))
-
-    truncate(plain_text, length: limit).html_safe
-  end
 end
